ab73a090f8ee683ffcc74971909fab5435c52a42
Alexander Hermans <hermans@vision.rwth-aachen.de>
2016-12-07 00:10:34 +0100

    Some more stuff in the pymongo playground.

--- pymongo_test.py
+++ pymongo_test.py
@@ -13,6 +13,35 @@ import pymongo
 import signal
 import datetime
 
+
+
+disk_lines = [
+'/dev/sda1      ext4  902G  805G   51G  95% /',
+'/dev/sda1      ext4  902G  805G   51G  95% /',
+'udev                          devtmpfs   32G   12K   32G   1% /dev',
+'tmpfs                         tmpfs     6.3G  1.6M  6.3G   1% /run',
+'/dev/sda3                     ext4      200G  159G   31G  84% /',
+'none                          tmpfs     4.0K     0  4.0K   0% /sys/fs/cgroup',
+'none                          tmpfs     5.0M     0  5.0M   0% /run/lock',
+'none                          tmpfs      32G  479M   31G   2% /run/shm',
+'none                          tmpfs     100M  104K  100M   1% /run/user',
+'/dev/sda1                     vfat       47M  3.4M   43M   8% /boot/efi',
+'/dev/sdb1                     ext4      3.6T  2.9T  603G  83% /work',
+'udev                          devtmpfs   16G   12K   16G   1% /dev',
+'tmpfs                         tmpfs     3.2G  1.6M  3.2G   1% /run',
+'/dev/sda5                     ext4      202G  170G   22G  89% /',
+'none                          tmpfs     4.0K     0  4.0K   0% /sys/fs/cgroup',
+'none                          tmpfs     5.0M     0  5.0M   0% /run/lock',
+'none                          tmpfs      16G  508K   16G   1% /run/shm',
+'none                          tmpfs     100M  228K  100M   1% /run/user',
+'/dev/sda1                     vfat      496M  3.7M  493M   1% /boot/efi',
+'/dev/sdb1                     ext4      2.7T  1.7T  953G  64% /work']
+
+for l in disk_lines:
+    res = re.search('(?P<dev>\S+) *(?P<type>\S+) *(?P<size>\S+) *(?P<used>\S+) *(\S+) *(?P<mount>\S+)', l)
+    print(res.group('dev'),res.group('type'),res.group('size'),res.group('used'),res.group('mount'))
+
+
 proc_lines = [
 '|    0       914    G   compiz                                         218MiB |',
 '|    0      7017    G   ...ves-passed-by-fd --v8-snapshot-passed-by-    94MiB |',
@@ -25,8 +54,6 @@ proc_lines = [
 for l in proc_lines:
     res = re.search('\| *(?P<gpu_id>\d+) *(?P<pid>\d*) *[CG]* *([\S|\s]*?)\s+(?P<mem>\d*)[MiB|\s\|]', l)
     print(res.group('gpu_id'),res.group('pid'),res.group('mem'))
-
-exit()
 
 mongo_client = pymongo.MongoClient(host='localhost', port=27018)
 mongo_client['admin'].authenticate('admin','test123')
