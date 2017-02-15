ea010f7a0a3c195a6e7f905f73fc5e37cdee4888
Alexander Hermans <Alexander.Hermans0@gmail.com>
2017-02-15 03:18:15 +0100

    Initial testing version of the gpu parser.

--- StatusCollector/Parsers/GpuParser.py
+++ StatusCollector/Parsers/GpuParser.py
@@ -1,6 +1,36 @@
 import StatusCollector as sc
 
+import pynvml
+
 class GpuParser(sc.Parser):
     def __init__(self):
-        print('No GPU found')
-        print('testing')
+        try:
+            pynvml.nvmlInit()
+            self.nvml_initialized = True
+        except pynvml.NVMLError_LibraryNotFound:
+            self.nvml_initialized = False
+
+    def __del__(self):
+        if self.nvml_initialized:
+            pynvml.nvmlShutdown()
+
+    def parse(self):
+        if self.nvml_initialized:
+            #Get number of GPUs
+            deviceCount = pynvml.nvmlDeviceGetCount()
+            print(deviceCount)
+            # For a fixed set of runs with pauses
+                #For each GPU
+                    # Get Fan speed
+                    # Get the temp
+                    # Get current power
+                    # Get max power
+                    # Get current memory
+                    # Get max memory
+                    # Get the load percentage
+                    # Parse the running processes on this gpu, their memory and their owner
+
+            # Aggregate the values over some runs.
+        else:
+            print('Cannot load driver')
+        # Return the json
