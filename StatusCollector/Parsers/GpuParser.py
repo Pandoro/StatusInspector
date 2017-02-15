331978c296630c53d94536c0e6a87b14cc2b5485
Alexander Hermans <Alexander.Hermans0@gmail.com>
2017-02-16 00:19:17 +0100

    Rough working framework for parsing the gpu info.

--- StatusCollector/Parsers/GpuParser.py
+++ StatusCollector/Parsers/GpuParser.py
@@ -5,6 +5,8 @@ try:
 except ImportError:
     pynvml = None
 
+import time
+
 class GpuParser(sc.Parser):
     def __init__(self):
         self.nvml_initialized = False
@@ -15,6 +17,11 @@ class GpuParser(sc.Parser):
             except pynvml.NVMLError_LibraryNotFound:
                 pass
 
+        #TODO make these parameters once it becomes clear how parameters will be dealt with.
+        self.runs = 5
+        self.ms_between_runs = 10
+        self.aggregation_function = max
+
     def description(self):
         return 'Parse the GPU usage.'
 
@@ -23,26 +30,51 @@ class GpuParser(sc.Parser):
             pynvml.nvmlShutdown()
 
     def parse(self):
+        res = {}
         if self.nvml_initialized:
-            #Get number of GPUs
+            # Get number of GPUs
             deviceCount = pynvml.nvmlDeviceGetCount()
-            print(deviceCount)
+
             # For a fixed set of runs with pauses
-                #For each GPU
-                    # Get Fan speed
-                    # Get the temp
-                    # Get current power
-                    # Get max power
-                    # Get current memory
-                    # Get max memory
-                    # Get the load percentage
-                    # Parse the running processes on this gpu, their memory and their owner
+            run_results = [[] for _ in range(deviceCount)]
+            for r in range(self.runs):
+                # Get the info for each GPU
+                for i in range(deviceCount):
+                    run_results[i].append(self.parse_single_gpu(i))
+
+                if r != self.runs-1:
+                    # Pause inbetween runs
+                    time.sleep(self.ms_between_runs/1000)
 
             # Aggregate the values over some runs.
+            res['gpus'] = self.aggregate_gpu_info(run_results)
+
         else:
             if pynvml is not None:
-                print('Cannot load driver')
+                res['error'] = 'nvml wasn\'t initialized properly'
             else:
-                print('pynvml missing, please install')
+                res['error'] = 'pynvml missing'
 
-        # Return the json
+        return {'Gpu' : res}
+
+    def parse_single_gpu(self, gpu_index):
+        # Get the actual GPU handle.
+        # Get Fan speed
+        # Get the temp
+        # Get current power
+        # Get max power
+        # Get current memory
+        # Get max memory
+        # Get the load percentage
+        # Parse the running processes on this gpu, their memory and their owner
+        return {'fan' : 99, 'load' : 50}
+
+    def aggregate_gpu_info(self, run_results):
+        result = [{} for _ in range(len(run_results))]
+        for i,g in enumerate(run_results):
+            keys = g[0].keys()
+            for k in keys:
+                vals = [g_i[k] for g_i in g]
+                result[i][k] = self.aggregation_function(vals)
+
+        return result
