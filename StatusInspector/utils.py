c7482d3fd11774b01ff84f12be93d39b0b406a6f
Alexander Hermans <Alexander.Hermans0@gmail.com>
2017-02-16 18:41:25 +0100

    Added a uid to username utility.

--- StatusInspector/utils.py
+++ StatusInspector/utils.py
@@ -4,4 +4,8 @@ import pwd
 def pid_to_username(pid):
     proc_stat_file = os.stat('/proc/{}'.format(pid))
     uid = proc_stat_file.st_uid
+    return uid_to_username(uid)
+
+
+def uid_to_username(uid):
     return pwd.getpwuid(uid)[0]
