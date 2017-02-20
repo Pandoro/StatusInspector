c806684b285b47789eab3681ffa9182cdbfb2fa7
Alexander Hermans <Alexander.Hermans0@gmail.com>
2017-02-20 18:26:28 +0100

    Start of CPUInfo, commiting for later on tonight.

--- StatusInspector/Parsers/CpuParser.py
+++ StatusInspector/Parsers/CpuParser.py
@@ -2,7 +2,20 @@ import StatusInspector as stasi
 
 class CpuParser(stasi.Parser):
     def __init__(self):
-        pass
+        #TODO make these parameters once it becomes clear how parameters will be dealt with.
+        self.runs = 50
+        self.ms_between_runs = 10
+        self.aggregation_function = max
 
     def description(self):
         return 'Parse the CPU usage.'
+
+    def parse(self):
+        #TODO parse this from /proc/cpuinfo.
+        #Look at the man page.
+        #Currently the values don't seem to add up to the same thing.
+        #I guess this is due to a variable cpu speed as clearly shown by /proc/cpuinfo.
+        #That might also be interesting.
+
+        #Probably we don't want to parse this user specific.
+        return {'data' : None}
